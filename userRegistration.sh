80d693493a8e2399b3163e326322d1f2b1d6f878
anish <anishsalgia1997@gmail.com>
2020-12-13 17:49:21 +0530

    Refactor: Code for firstName

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
 echo "Welcome to Pattern Matching User Registration"
+read -p "Enter First Name" checkFirstName
+
+Pattern=^[A-Z][a-zA-Z]{2}
+
+if [[ $checkFirstName =~ $Pattern ]]
+then
+	echo "$checkFirstName is valid"
+else 
+	echo "Invalid First Name"
+fi
