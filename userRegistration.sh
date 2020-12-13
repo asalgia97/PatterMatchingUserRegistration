c50430b805f951274a3517b5ad6dcb67c048caa7
anish <anishsalgia1997@gmail.com>
2020-12-13 18:03:13 +0530

    Refactor: Code for Email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
+shopt -s extglob
 echo "Welcome to Pattern Matching User Registration"
 read -p "Enter First Name" checkFirstName
 read -p "Enter Last Name" checkLastName
+read -p "Enter Email" Email
 Pattern=^[A-Z][a-zA-Z]{2}
 Pattern2=^[A-Z][a-zA-Z]{2}
+emailPattern=^[a-z]+([.][a-z]+)?@[a-z]+.[a-z]+([.][a-z]+)?$
 if [[ $checkFirstName =~ $Pattern ]]
 then
 	echo "$checkFirstName is valid"
@@ -15,4 +18,10 @@ then
 	echo "$checkLastName is valid"
 else
 	echo "Invalid Last Name"
+fi
+if [[ $Email =~ $emailPattern ]]
+then
+	echo "$email is valid"
+else
+	echo "Invalid Email"
 fi
